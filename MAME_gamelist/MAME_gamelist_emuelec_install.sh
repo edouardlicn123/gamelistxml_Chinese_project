983fd3aafd440e5bebbc5edec3acbc4efd9dd3a3
“edouardlicn” <edouardlicn@gmail.com>
2022-05-08 16:43:07 +0800

    first update

---
+++ MAME_gamelist/MAME_gamelist_emuelec_install.sh
@@ -0,0 +1,11 @@
+mv ~/roms/mame/gamelist.xml ~/roms/mame/gamelist.xml.oldbak
+mv ~/roms/fbneo/gamelist.xml ~/roms/fbneo/gamelist.xml.oldbak
+mv ~/roms/arcade/gamelist.xml ~/roms/arcade/gamelist.xml.oldbak   
+mv ~/roms/atomiswave/gamelist.xml ~/roms/arcade/gamelist.xml.oldbak
+mv ~/roms/naomi/gamelist.xml ~/roms/arcade/gamelist.xml.oldbak
+
+ln ./MAME_gamelist.xml ~/roms/mame/gamelist.xml
+ln ./MAME_gamelist.xml ~/roms/fbneo/gamelist.xml
+ln ./MAME_gamelist.xml ~/roms/arcade/gamelist.xml
+ln ./MAME_gamelist.xml ~/roms/atomiswave/gamelist.xml
+ln ./MAME_gamelist.xml ~/roms/naomi/gamelist.xml
---
+++ MAME_gamelist/MAME_gamelist_emuelec_uninstall.sh
@@ -0,0 +1,11 @@
+rm ~/roms/mame/gamelist.xml
+rm ~/roms/fbneo/gamelist.xml
+rm ~/roms/arcade/gamelist.xml   
+rm ~/roms/atomiswave/gamelist.xml 
+rm ~/roms/naomi/gamelist.xml 
+
+mv ~/roms/mame/gamelist.xml.oldbak ~/roms/mame/gamelist.xml
+mv ~/roms/fbneo/gamelist.xml.oldbak ~/roms/fbneo/gamelist.xml
+mv ~/roms/arcade/gamelist.xml.oldbak ~/roms/arcade/gamelist.xml
+mv ~/roms/arcade/gamelist.xml.oldbak ~/roms/atomiswave/gamelist.xml
+mv ~/roms/arcade/gamelist.xml.oldbak ~/roms/naomi/gamelist.xml
