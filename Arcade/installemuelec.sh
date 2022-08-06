812d415ac9fed10246811d724c3c4d8940d214e3
“edo” <edouardlicn@gmail.com>
2022-08-07 03:31:25 +0800

    update

--- Arcade/installemuelec.sh
+++ Arcade/installemuelec.sh
@@ -1,20 +1,13 @@
 
-rm ~/roms/mame/gamelist.xml.old
-rm ~/roms/fbneo/gamelist.xml.old
-rm ~/roms/arcade/gamelist.xml.old   
-rm ~/roms/atomiswave/gamelist.xml.old 
-rm ~/roms/naomi/gamelist.xml.old     
-rm ~/roms/cps1/gamelist.xml.old   
-rm ~/roms/cps2/gamelist.xml.old 
-rm ~/roms/cps3/gamelist.xml.old 
-rm ~/roms/neogeo/gamelist.xml.old
 
-ln ./MAME_gamelist.xml ~/roms/mame/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/fbneo/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/arcade/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/atomiswave/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/naomi/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/cps1/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/cps2/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/cps3/gamelist.xml
-ln ./MAME_gamelist.xml ~/roms/neogeo/gamelist.xml
+
+ln ./Arcade_gamelist.xml ~/roms/mame/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/fbneo/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/arcade/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/atomiswave/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/aw_dcported/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/naomi/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/cps1/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/cps2/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/cps3/gamelist.xml
+ln ./Arcade_gamelist.xml ~/roms/neogeo/gamelist.xml
--- Arcade/uninstallemuelec.sh
+++ Arcade/uninstallemuelec.sh
@@ -2,6 +2,7 @@ rm ~/roms/mame/gamelist.xml
 rm ~/roms/fbneo/gamelist.xml
 rm ~/roms/arcade/gamelist.xml   
 rm ~/roms/atomiswave/gamelist.xml 
+rm ~/roms/aw_dcported/gamelist.xml
 rm ~/roms/naomi/gamelist.xml     
 rm ~/roms/cps1/gamelist.xml   
 rm ~/roms/cps2/gamelist.xml 
